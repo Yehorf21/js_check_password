5b053cb67eb5204ae94726f81aec419712c1047c
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-05-11 13:00:14 +0300

    solution

--- src/checkPassword.js
+++ src/checkPassword.js
@@ -5,13 +5,18 @@
  *
  * @returns {boolean}
  */
+
+/* eslint-disable max-len */
+
 function checkPassword(password) {
   // eslint-disable-next-line
   const validPasswordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,16}$/;
-  const cyrillicValidation = /^((?![А-Яа-я]).)*$/;
+
+  /* it was specified in the description on github that "accepts only letters of the Latin alphabet Aa-Zz" */
+  // const cyrillicValidation = /^((?![А-Яа-я]).)*$/;
 
   // eslint-disable-next-line
-  if (password.match(validPasswordRegex) && password.match(cyrillicValidation)) {
+  if (password.match(validPasswordRegex)) {
     return true;
   }
 
--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,38 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = checkPassword('12345678');
 
+    expect(typeof result).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid password with 8 characters`, () => {
+  it('expects 8 characters', () => {
+    const result = checkPassword('12345');
 
+    expect(result).toBeFalsy();
   });
 
-  // write more tests here
+  it('includes at least 1 digit, 1 special and 1 uppercase characters', () => {
+    const result = checkPassword('12345!qw');
+
+    expect(result).toBeFalsy();
+  });
+
+  it('shorter than 16 characters', () => {
+    const result = checkPassword('12345!Qwqwerttyyieodj7163');
+
+    expect(result).toBeFalsy();
+  });
+
+  it('accepts letters of the Latin alphabet only', () => {
+    const result = checkPassword('12345!Qй');
+
+    expect(result).toBeFalsy();
+  });
+
+  it('returns true for valid passwords', () => {
+    const result = checkPassword('12345!Qw');
+
+    expect(result).toBeTruthy();
+  });
 });
